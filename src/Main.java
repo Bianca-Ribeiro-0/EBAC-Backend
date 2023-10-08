7fcdacac0db77c44434e68ccef34ebf637a77afd
Bianca-Ribeiro-0 <biancamelamed7@gmail.com>
2023-10-08 19:59:10 -0300

    exercicio classes

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String args[]) {
+        System.out.println("Olá, este é o exercicio");
+        Pessoa pessoa = new Pessoa();
+        pessoa.Falar("olá olá");
+    }
+
+}
