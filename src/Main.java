b07b0adf0f9df93855736896bbd3c8385ec1ba01
Bianca-Ribeiro-0 <biancamelamed7@gmail.com>
2023-10-08 21:46:09 -0300

    adicionando calculo media de alunos

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
-    public static void main (String args[]) {
-        System.out.println("Olá, este é o exercicio");
-        Pessoa pessoa = new Pessoa();
-        pessoa.Falar("olá olá");
+    public static void main(String args[]) {
+        MediaAlunos();
     }
 
+    private static void MediaAlunos() {
+        int num1 = 10;
+        int num2= 8;
+        float num3= 6.5f;
+        float num4 = 10;
+
+        float num5 = (num1 + num2 + num3 + num4)/4;
+        System.out.println("A média é:" + num5);
+    }
 }
