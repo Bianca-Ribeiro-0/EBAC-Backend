e0d22ad66fad85bd8a8a7f2ab36051bdd784ea93
Bianca-Ribeiro-0 <biancamelamed7@gmail.com>
2023-10-13 14:16:52 -0300

    exercitando casting

---
+++ src/Ex2.java
@@ -0,0 +1,9 @@
+public class Ex2 {
+    public static void main(String[] args) {
+        // criar uma classe e ler um valor numérico, colocar em uma variável primitiva e fazer a conversão wrapper dela
+        long num1 = 10;
+        Long num2 = num1;
+        System.out.println(num2);
+        System.out.println(num1);
+    }
+}
