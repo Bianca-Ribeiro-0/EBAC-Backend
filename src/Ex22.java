149bb6a77f2f304a201e5f9ff2fb57cefd4cd042
bianca-ribeiro-0 <biancamelamed7@gmail.com>
2023-11-19 21:03:30 -0300

    adicionando exercicio 22

---
+++ src/Ex22.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Ex22 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        List<Pessoa> lista = pessoa.populaPessoas();
+
+        lista.stream()
+                .filter(p -> "Feminino".equals(p.getSexo()))
+                .forEach(System.out::println);
+    }
+}
